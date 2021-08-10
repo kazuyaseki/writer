5051123fbba19036afef2f5766e41d049e179723
kazuyaseki <track613armory@gmail.com>
2021-08-11 00:18:34 +0900

    tategaki

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,41 +1,33 @@
 import { styled } from '@stitches/react';
 import { useEditor, EditorContent, Editor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import { useEffect } from 'react';
+
+const extention = StarterKit.extend({
+  addKeyboardShortcuts() {
+    return {
+      'Cmd+l': () => {
+        console.log(this.editor);
+        return this.editor.commands.insertContent('ahaaha');
+      },
+    };
+  },
+});
 
 export const MyEditor = () => {
   const editor = useEditor({
     extensions: [StarterKit],
     content: `
-      <h2>
-        Hi there,
-      </h2>
-      <p>
-        this is a <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-      </p>
-      <ul>
-        <li>
-          That’s a bullet list with one …
-        </li>
-        <li>
-          … or two list items.
-        </li>
-      </ul>
-      <p>
-        Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-      </p>
-      <pre><code class="language-css">body {
-  display: none;
-}</code></pre>
-      <p>
-        I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-      </p>
-      <blockquote>
-        Wow, that’s amazing. Good work, boy! 👏
-        <br />
-        — Mom
-      </blockquote>
+      ここから書き始めてみてね<br />
+    何かが起きるかも
     `,
+    autofocus: true,
   });
+
+  useEffect(() => {
+    editor?.on('click', (e: any) => console.log(e));
+    // editor?.registerPlugin
+  }, []);
 
   return (
     <div>
@@ -46,8 +38,14 @@ export const MyEditor = () => {
 };
 
 const StyledEditorContent = styled(EditorContent, {
-  border: '2px solid #666',
-  padding: '8px',
+  writingMode: 'vertical-rl',
+  textOrientation: 'upright',
+  width: '100%',
+  height: '100%',
+  '& div': {
+    padding: '8px 20px',
+    outline: 'none',
+  },
 });
 
 const MenuBar: React.VFC<{ editor: Editor | null }> = ({ editor }) => {
